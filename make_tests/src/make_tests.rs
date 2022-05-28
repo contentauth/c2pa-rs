1a938eba96d2d071833bb718004e0f6d9ebb08bd
Gavin Peacock <gpeacock@adobe.com>
2022-05-27 23:29:06 -0700

    Move make_tests to bin and drive it from a config file

--- make_tests/src/make_tests.rs
+++ make_tests/src/make_tests.rs
@@ -11,6 +11,8 @@
 // specific language governing permissions and limitations under
 // each license.
 
+//! Constructs a set of test images using a configuration script
+//!
 use anyhow::{Context, Result};
 
 use c2pa::{
@@ -30,71 +32,117 @@ use std::{
     path::{Path, PathBuf},
 };
 
-const GENERATOR: &str = "make_tests";
-const USER: &str = "Joe Bloggs";
+use serde::Deserialize;
 
 const IMAGE_WIDTH: u32 = 2048;
 const IMAGE_HEIGHT: u32 = 1365;
 
-/**
-Patch new content into a file
-path - path to file to be patched
-search_bytes - bytes to be replaced
-replace_bytes - replacement bytes
-*/
-pub fn patch_file(path: &std::path::Path, search_bytes: &[u8], replace_bytes: &[u8]) -> Result<()> {
-    let mut buf = fs::read(path)?;
-
-    if let Some(splice_start) = find_bytes(&buf, search_bytes) {
-        buf.splice(
-            splice_start..splice_start + search_bytes.len(),
-            replace_bytes.iter().cloned(),
-        );
-    } else {
-        return Err(Error::NotFound.into());
-    }
-
-    fs::write(path, &buf)?;
-
-    Ok(())
+#[derive(Debug, Deserialize)]
+pub struct Recipe {
+    pub op: String,
+    pub parent: Option<String>,
+    pub ingredients: Option<Vec<String>>,
+    pub output: String,
 }
 
+/// Configuration
+#[derive(Debug, Deserialize)]
+#[serde(default)]
+pub struct Config {
+    /// The signing algorithm to use
+    pub alg: String,
+    /// A url to a time stamp authority if desired
+    pub tsa: Option<String>,
+    /// The output folder for the generated files
+    pub output_path: String,
+    /// Extension to add to filenames if none was given
+    pub default_ext: String,
+    /// A name for a Creative Work Author assertion
+    pub author: Option<String>,
+    /// the list of recipes for test files
+    pub recipes: Vec<Recipe>,
+}
+
+// Defaults for Config
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            alg: "ps256".to_owned(),
+            tsa: Some("http://timestamp.digicert.com".to_owned()),
+            output_path: "target/images".to_owned(),
+            default_ext: "jpg".to_owned(),
+            author: None,
+            recipes: Vec::new(),
+        }
+    }
+}
+
+/// Tool for building test case images for C2PA
 pub struct MakeTests {
+    config: Config,
     output_dir: PathBuf,
 }
 
 impl MakeTests {
-    pub fn new(path: &Path) -> Self {
+    pub fn new(config: Config) -> Self {
+        let output = config.output_path.to_owned();
         Self {
-            output_dir: PathBuf::from(path),
+            config,
+            output_dir: PathBuf::from(output),
         }
     }
 
-    pub fn output_dir(&self) -> PathBuf {
-        self.output_dir.to_owned()
-    }
-
+    /// Makes a full path from a filename or path
+    ///
+    /// If there is no parent, prepend the output path
+    /// If there is no extension, use the default
     fn make_path(&self, s: &str) -> PathBuf {
-        //let output_dir = unsafe { OUTPUT_FOLDER.as_ref().unwrap().lock().unwrap().to_string() };
-        let mut path_buf = PathBuf::from(&self.output_dir);
-        path_buf.push(s);
+        let mut path_buf = PathBuf::from(s);
+        // parent() tends to return an empty string instead of None
+        let has_path = match path_buf.parent() {
+            Some(p) => p.to_string_lossy().len() > 0,
+            None => false,
+        };
+        // if we just have a filename, then assume it is in the output folder
+        if !has_path {
+            path_buf = PathBuf::from(&self.output_dir);
+            path_buf.push(s);
+        }
+        // add the default extension is none is supplied
         if path_buf.extension().is_none() {
-            path_buf.set_extension("jpg");
+            path_buf.set_extension(&self.config.default_ext);
         }
         path_buf
     }
 
-    // create a test image with optional source and ingredients, out to dest
-    pub fn make_image(
-        &self,
-        src: Option<&str>,
-        ing: Option<&Vec<&str>>,
-        dst: &str,
-        alg: &str,
-        tsa: Option<String>,
-    ) -> Result<()> {
-        let dst_path = &self.make_path(dst);
-        println!("creating {:?}", dst_path);
+    /// Patches new content into a file
+    ///
+    /// # Parameters
+    /// path - path to file to be patched
+    /// search_bytes - bytes to be replaced
+    /// replace_bytes - replacement bytes
+    fn patch_file(path: &std::path::Path, search_bytes: &[u8], replace_bytes: &[u8]) -> Result<()> {
+        let mut buf = fs::read(path)?;
+
+        if let Some(splice_start) = find_bytes(&buf, search_bytes) {
+            buf.splice(
+                splice_start..splice_start + search_bytes.len(),
+                replace_bytes.iter().cloned(),
+            );
+        } else {
+            return Err(Error::NotFound.into());
+        }
+
+        fs::write(path, &buf)?;
+
+        Ok(())
+    }
+
+    /// Creates a test image with optional source and ingredients, out to dest
+    fn make_image(&self, recipe: &Recipe) -> Result<PathBuf> {
+        let src = recipe.parent.as_deref();
+        let dst_path = self.make_path(&recipe.output);
+        println!("Creating {:?}", dst_path);
         // keep track of all actions here
         let mut actions = Actions::new();
 
@@ -103,13 +151,16 @@ impl MakeTests {
             title: None,
         };
 
-        let mut manifest = Manifest::new(GENERATOR.to_string());
+        let generator = format!("{}/{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+        let mut manifest = Manifest::new(generator);
         manifest.set_vendor("contentauth".to_owned()); // needed for generating error cases below
 
-        let creative_work =
-            CreativeWork::new().add_author(SchemaDotOrgPerson::new().set_name(USER.to_owned())?)?;
+        if let Some(user) = self.config.author.as_ref() {
+            let creative_work = CreativeWork::new()
+                .add_author(SchemaDotOrgPerson::new().set_name(user.to_owned())?)?;
 
-        manifest.add_assertion(&creative_work)?;
+            manifest.add_assertion(&creative_work)?;
+        }
 
         // process parent first
         let mut img = match src {
@@ -125,7 +176,7 @@ impl MakeTests {
 
                 // load the image for editing
                 let mut img =
-                    image::open(&src_path).context(format!("opening image A {:?}", src_path))?;
+                    image::open(&src_path).context(format!("opening parent {:?}", src_path))?;
 
                 // adjust brightness to show we made an edit
                 img = img.brighten(30);
@@ -158,7 +209,7 @@ impl MakeTests {
         };
 
         // then add all ingredients
-        if let Some(ing_vec) = ing {
+        if let Some(ing_vec) = &recipe.ingredients {
             // scale ingredients to paste in top row of the image
             let width = match ing_vec.len() as u32 {
                 0 | 1 => img.width() / 2,
@@ -172,7 +223,7 @@ impl MakeTests {
 
                 // get the bits of the ingredient, resize it and overlay it on the base image
                 let img_ingredient =
-                    image::open(&ing_path).context(format!("opening image I {:?}", ing_path))?;
+                    image::open(&ing_path).context(format!("opening ingredient {:?}", ing_path))?;
                 let img_small = img_ingredient.thumbnail(width, height);
                 image::imageops::overlay(&mut img, &img_small, x, 0);
 
@@ -193,49 +244,52 @@ impl MakeTests {
         }
 
         // save the changes to the image as our target file
-        img.save(dst_path)?;
+        img.save(&dst_path)?;
 
         // add all our actions as an assertion now.
         manifest.add_assertion(&actions)?; // extra get required here, since actions is an array
 
         // now create store; sign claim and embed in target
         let temp_dir = tempdir()?;
-        let (signer, _) = get_signer_by_alg(&temp_dir.path(), alg, tsa);
+        let (signer, _) =
+            get_signer_by_alg(&temp_dir.path(), &self.config.alg, self.config.tsa.clone());
 
-        manifest.embed(dst_path, dst_path, signer.as_ref())?;
+        manifest.embed(&dst_path, &dst_path, signer.as_ref())?;
 
-        println!("{}", ManifestStore::from_file(dst_path)?);
-
-        Ok(())
+        Ok(dst_path)
     }
 
-    // make an off the golden path image from an existing image with a claim
-    fn make_ogp(&self, src: &str, dst: &str) -> Result<()> {
-        println!("creating OGP {}", dst);
+    /// makes an off the golden path image from an existing image with a claim
+    fn make_ogp(&self, recipe: &Recipe) -> Result<PathBuf> {
+        let src = recipe.parent.as_deref().unwrap_or_default();
         let src_path = &self.make_path(src);
-        let dst_path = &self.make_path(dst);
+        let dst_path = self.make_path(recipe.output.as_str());
+        println!("Creating OGP {:?}", dst_path);
+
         let jumbf = jumbf_io::load_jumbf_from_file(&PathBuf::from(src_path))
             .context(format!("loading OGP {:?}", src_path))?;
         // save the edited image to our destination file
         let mut img = image::open(&Path::new(src_path))
             .context(format!("loading OGP image{:?}", src_path))?;
         img = img.grayscale();
-        img.save(dst_path)
-            .context(format!("saving OGP image{:?}", dst_path))?;
+        img.save(&dst_path)
+            .context(format!("saving OGP image{:?}", &dst_path))?;
         // write the original claim data to the edited image
-        jumbf_io::save_jumbf_to_file(
-            &jumbf,
-            &PathBuf::from(dst_path),
-            Some(&PathBuf::from(dst_path)),
-        )
-        .context(format!("OGP save_jumbf_to_file {:?}", dst_path))?;
+        jumbf_io::save_jumbf_to_file(&jumbf, &PathBuf::from(&dst_path), Some(&dst_path))
+            .context(format!("OGP save_jumbf_to_file {:?}", &dst_path))?;
         // The image library does not preserve any metadata so we have to write it ourselves.
         // todo: should preserve all metadata and update instanceId.
-        Ok(())
+        Ok(dst_path)
     }
 
-    fn make_err(&self, src: &str, err: &str) -> Result<()> {
-        let (search_bytes, replace_bytes) = match err {
+    /// Generates various error conditions
+    fn make_err(&self, recipe: &Recipe) -> Result<PathBuf> {
+        let op = recipe.op.as_str();
+        let src = recipe.parent.as_deref().unwrap_or_default();
+        let dst_path = self.make_path(recipe.output.as_str());
+        println!("Creating Error op={} {:?}", op, dst_path);
+
+        let (search_bytes, replace_bytes) = match op {
             // modify the XMP (change xmp magic id value) - this should cause a data hash mismatch (OTGP)
             "dat" => (
                 b"W5M0MpCehiHzreSzNTczkc9d".as_bytes(),
@@ -251,111 +305,55 @@ impl MakeTests {
             // modify a uri to a manifest so the manifest cannot be found (missing manifest)
             "clm" => (
                 b"c2pa_manifest\xA3\x63url\x78\x4aself#jumbf=/c2pa/contentauth".as_bytes(),
-                b"c2pa_manifest\xA3\x63url\x78\x4aself#jumbf=/c2pa/contentauth".as_bytes(),
+                b"c2pa_manifest\xA3\x63url\x78\x4aself#jumbf=/c2pa/contentbeef".as_bytes(),
             ),
             // modify the provenance uri so that is references a non-existing manifest
             "prv" => (
                 b"dcterms:provenance=\"self#jumbf=/c2pa/contentauth".as_bytes(),
-                b"dcterms:provenance=\"self#jumbf=/c2pa/contentauth".as_bytes(),
+                b"dcterms:provenance=\"self#jumbf=/c2pa/contentbeef".as_bytes(),
             ),
             _ => panic!("bad parameter"),
         };
 
-        let dst = format!("E-{}-{}", err, src);
-        std::fs::copy(&self.make_path(src), &self.make_path(&dst))
-            .context("copying for make_err")?;
-        patch_file(&self.make_path(&dst), search_bytes, replace_bytes)
-            .context(format!("patching {}", err))?;
+        std::fs::copy(&self.make_path(src), &dst_path).context("copying for make_err")?;
 
+        Self::patch_file(&dst_path, search_bytes, replace_bytes)
+            .context(format!("patching {}", op))?;
+
+        Ok(dst_path)
+    }
+
+    /// copies a file from the parent to the output
+    fn make_copy(&self, recipe: &Recipe) -> Result<PathBuf> {
+        let dst_path = self.make_path(recipe.output.as_str());
+        println!("Copying {:?}", dst_path);
+        let src = recipe.parent.as_deref().unwrap_or_default();
+        let dst = recipe.output.as_str();
+        std::fs::copy(src, &dst_path).context(format!("copying {} to {}", src, dst))?;
+        Ok(dst_path)
+    }
+
+    /// Runs a list of recipes
+    pub fn run(&self) -> Result<()> {
+        if !self.output_dir.exists() {
+            std::fs::create_dir_all(&self.output_dir).context("Can't create output folder")?;
+        };
+
+        let recipes = &self.config.recipes;
+        for recipe in recipes {
+            let dst_path = match recipe.op.as_str() {
+                "make" => self.make_image(recipe)?,
+                "ogp" => self.make_ogp(recipe)?,
+                "dat" | "sig" | "uri" | "clm" | "prv" => self.make_err(recipe)?,
+                "copy" => self.make_copy(recipe)?,
+                _ => return Err(Error::BadParam(recipe.op.to_string()).into()),
+            };
+            let manifest_store = ManifestStore::from_file(&dst_path);
+
+            if recipe.op.as_str() != "copy" {
+                println!("{}", manifest_store?);
+            }
+        }
         Ok(())
     }
-}
-
-pub fn make_tests(output_folder: &Path, alg: &str, tsa: Option<String>) -> Result<()> {
-    // destination folder for this content
-    let mt = MakeTests::new(output_folder);
-    let data_dir = mt.output_dir();
-
-    if !data_dir.exists() {
-        std::fs::create_dir_all(&data_dir).expect("Can't create C2PA data directory");
-    };
-
-    // copy A and I source images into destination folder
-    // these images should have no claims
-    std::fs::copy("sdk/tests/fixtures/IMG_0003.jpg", &mt.make_path("A.jpg"))
-        .context("error copying A")?;
-    std::fs::copy("sdk/tests/fixtures/P1000827.jpg", &mt.make_path("I.jpg"))
-        .context("error copying I")?;
-
-    // --------------------------------------------------------------------
-
-    //make_cai(None, Some(&vec!["PS.svg"]), "CIPS")?;
-    mt.make_image(None, None, "C", alg, tsa.clone())?;
-    mt.make_image(Some("A"), None, "CA", alg, tsa.clone())?;
-    mt.make_image(Some("CA"), None, "CACA", alg, tsa.clone())?;
-    mt.make_image(None, Some(&vec!["I"]), "CI", alg, tsa.clone())?;
-    mt.make_image(None, Some(&vec!["I", "I"]), "CII", alg, tsa.clone())?;
-    mt.make_image(
-        None,
-        Some(&vec!["I", "I", "I", "I", "I"]),
-        "CIIIII",
-        alg,
-        tsa.clone(),
-    )?;
-    mt.make_image(Some("A"), Some(&vec!["I"]), "CAI", alg, tsa.clone())?;
-    mt.make_image(Some("A"), Some(&vec!["CA"]), "CAICA", alg, tsa.clone())?;
-    mt.make_image(None, Some(&vec!["CA"]), "CICA", alg, tsa.clone())?;
-    mt.make_image(Some("CA"), Some(&vec!["CAI"]), "CAICAI", alg, tsa.clone())?;
-    mt.make_image(None, Some(&vec!["CA"]), "CICA", alg, tsa.clone())?;
-    mt.make_image(
-        Some("CAICA"),
-        Some(&vec!["CICA"]),
-        "CACAICAICICA",
-        alg,
-        tsa.clone(),
-    )?;
-    mt.make_image(
-        None,
-        Some(&vec!["CA", "CA", "CA"]),
-        "CICACACA",
-        alg,
-        tsa.clone(),
-    )?;
-
-    mt.make_ogp("CA", "XCA")?;
-    mt.make_ogp("CI", "XCI")?;
-    mt.make_image(Some("CA"), Some(&vec!["XCI"]), "CAIXCI", alg, tsa.clone())?;
-    mt.make_image(
-        Some("XCA"),
-        Some(&vec!["XCI"]),
-        "CAXCAIXCI",
-        alg,
-        tsa.clone(),
-    )?;
-
-    mt.make_err("CA", "dat")?;
-    mt.make_err("CA", "sig")?;
-    mt.make_err("CA", "uri")?;
-    mt.make_err("CAICAI", "clm")?;
-    mt.make_err("CA", "prv")?;
-    mt.make_image(
-        None,
-        Some(&vec!["E-sig-CA"]),
-        "CIE-sig-CA",
-        alg,
-        tsa.clone(),
-    )?;
-    // inject an assertion error into a claim that has an accepted error
-    mt.make_err("CIE-sig-CA", "uri")?;
-
-    mt.make_image(
-        Some("A"),
-        Some(&vec!["C", "A", "I", "CA", "CI", "CAI", "CICA"]),
-        "CAIAIIICAICIICAIICICA",
-        alg,
-        tsa,
-    )?;
-    // // save the changes to the image and add the claim
-    println!("done");
-    Ok(())
 }
