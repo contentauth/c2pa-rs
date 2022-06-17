9fd79d994576f66a12f0ecf4ac258dae3d5ca148
Eric Scouten <scouten@adobe.com>
2022-06-17 13:48:47 -0700

    Update to latest c2pa Rust SDK (contentauth/c2patool#12)
    * Update to latest c2pa Rust SDK
    
    * Add backtrace logging when running unit tests
    
    * Remove obsolete .x509 folder generation.
    
    Co-authored-by: Gavin Peacock <gpeacock@adobe.com>

--- cli/tests/integration.rs
+++ cli/tests/integration.rs
@@ -56,8 +56,6 @@ fn tool_jpeg_no_report() -> Result<(), Box<dyn std::error::Error>> {
 
 #[test]
 fn tool_embed_jpeg_report() -> Result<(), Box<dyn std::error::Error>> {
-    generate_x509_temp_keys();
-
     Command::cargo_bin("c2patool")?
         .arg("sample/test.json")
         .arg("-p")
@@ -73,7 +71,6 @@ fn tool_embed_jpeg_report() -> Result<(), Box<dyn std::error::Error>> {
 /* remove this until the c2patool supports .c2pa write again
 #[test]
 fn tool_manifest_report() -> Result<(), Box<dyn std::error::Error>> {
-    generate_x509_temp_keys();
 
     // first export a c2pa file
     Command::cargo_bin("c2patool")?
@@ -92,21 +89,3 @@ fn tool_manifest_report() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 */
-
-fn generate_x509_temp_keys() {
-    let mut x509_path = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    x509_path.push(".x509");
-
-    std::fs::create_dir_all(&x509_path).expect("Can't create .x509 dir in repo");
-
-    // Test for existence of x509_path.temp_key.pub and .pem.
-
-    let priv_key_path = x509_path.join("temp_key.pem");
-    let sign_cert_path = x509_path.join("temp_key.pub");
-
-    if !(priv_key_path.exists() && sign_cert_path.exists()) {
-        // Creating the signer (which we don't use) has the side effect of
-        // creating temporary private key and signing certificate.
-        c2pa::get_temp_signer(&x509_path);
-    }
-}
