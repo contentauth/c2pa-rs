40fc8df617d99f7ffb76e61333856dcbafe1f882
mauricefisher64 <92736594+mauricefisher64@users.noreply.github.com>
2022-07-15 14:05:38 -0400

    Support for asynchronous signing of claims (#57)
    * Fix bug in verify_from_buffer
    
    * make_test fixes
    adjust signature box reserved size to account for large timestamps
    
    * change tsa to tsa_url
    
    * Fix make images in makefile
    
    * remove debug printlns
    
    * Async claim signing support
    
    * fix formatting
    
    * allow async validation with OpenSSL validators
    
    * simplify login in cose_pad
    
    * clippy fixes
    
    * cargo fmt fix
    
    * Remove old comment to resolve PR request
    
    * Restore `bmff` feature
    
    Co-authored-by: Gavin Peacock <gpeacock@adobe.com>
    Co-authored-by: Eric Scouten <scouten@adobe.com>

--- sdk/src/cose_sign.rs
+++ sdk/src/cose_sign.rs
@@ -16,7 +16,9 @@
 #![deny(missing_docs)]
 
 use ciborium::value::Value;
-use coset::{iana, CoseSign1, CoseSign1Builder, HeaderBuilder, Label, TaggedCborSerializable};
+use coset::{
+    iana, CoseSign1, CoseSign1Builder, Header, HeaderBuilder, Label, TaggedCborSerializable,
+};
 
 use crate::{
     claim::Claim,
@@ -25,6 +27,77 @@ use crate::{
     time_stamp::{cose_timestamp_countersign, make_cose_timestamp},
     Error, Result, Signer,
 };
+fn build_unprotected_header(
+    data: &[u8],
+    alg: &str,
+    certs: Vec<Vec<u8>>,
+    ta_url: Option<String>,
+    ocsp_val: Option<Vec<u8>>,
+) -> Result<(Header, Header)> {
+    let alg_id = match alg {
+        "ps256" => HeaderBuilder::new()
+            .algorithm(iana::Algorithm::PS256)
+            .build(),
+        "ps384" => HeaderBuilder::new()
+            .algorithm(iana::Algorithm::PS384)
+            .build(),
+        "ps512" => HeaderBuilder::new()
+            .algorithm(iana::Algorithm::PS512)
+            .build(),
+        "es256" => HeaderBuilder::new()
+            .algorithm(iana::Algorithm::ES256)
+            .build(),
+        "es384" => HeaderBuilder::new()
+            .algorithm(iana::Algorithm::ES384)
+            .build(),
+        "es512" => HeaderBuilder::new()
+            .algorithm(iana::Algorithm::ES512)
+            .build(),
+        "ed25519" => HeaderBuilder::new()
+            .algorithm(iana::Algorithm::EdDSA)
+            .build(),
+        _ => return Err(Error::UnsupportedType),
+    };
+
+    let sc_der_array_or_bytes = match certs.len() {
+        1 => Value::Bytes(certs[0].clone()), // single cert
+        _ => {
+            let mut sc_der_array: Vec<Value> = Vec::new();
+            for cert in certs {
+                sc_der_array.push(Value::Bytes(cert));
+            }
+            Value::Array(sc_der_array) // provide vec of certs when required
+        }
+    };
+
+    let mut unprotected = if let Some(url) = ta_url {
+        let cts = cose_timestamp_countersign(data, alg, &url)?;
+        let sigtst_vec = serde_cbor::to_vec(&make_cose_timestamp(&cts))?;
+        let sigtst_cbor = serde_cbor::from_slice(&sigtst_vec)?;
+
+        HeaderBuilder::new()
+            .text_value("x5chain".to_string(), sc_der_array_or_bytes)
+            .text_value("sigTst".to_string(), sigtst_cbor)
+    } else {
+        HeaderBuilder::new().text_value("x5chain".to_string(), sc_der_array_or_bytes)
+    };
+
+    // set the ocsp responder response if available
+    if let Some(ocsp) = ocsp_val {
+        let mut ocsp_vec: Vec<Value> = Vec::new();
+        let mut r_vals: Vec<(Value, Value)> = vec![];
+
+        ocsp_vec.push(Value::Bytes(ocsp));
+        r_vals.push((Value::Text("ocspVals".to_string()), Value::Array(ocsp_vec)));
+
+        unprotected = unprotected.text_value("rVals".to_string(), Value::Map(r_vals));
+    }
+
+    // build complete header
+    let unprotected_header = unprotected.build();
+
+    Ok((alg_id, unprotected_header))
+}
 
 /// Generate a COSE signature for a block of bytes which must be a valid C2PA
 /// claim structure.
@@ -87,75 +160,14 @@ pub(crate) fn cose_sign(signer: &dyn Signer, data: &[u8], box_size: usize) -> Re
 
     let alg = signer.alg().ok_or(Error::UnsupportedType)?;
 
-    let alg_id = match alg.as_ref() {
-        "ps256" => HeaderBuilder::new()
-            .algorithm(iana::Algorithm::PS256)
-            .build(),
-        "ps384" => HeaderBuilder::new()
-            .algorithm(iana::Algorithm::PS384)
-            .build(),
-        "ps512" => HeaderBuilder::new()
-            .algorithm(iana::Algorithm::PS512)
-            .build(),
-        "es256" => HeaderBuilder::new()
-            .algorithm(iana::Algorithm::ES256)
-            .build(),
-        "es384" => HeaderBuilder::new()
-            .algorithm(iana::Algorithm::ES384)
-            .build(),
-        "es512" => HeaderBuilder::new()
-            .algorithm(iana::Algorithm::ES512)
-            .build(),
-        "ed25519" => HeaderBuilder::new()
-            .algorithm(iana::Algorithm::EdDSA)
-            .build(),
-        _ => return Err(Error::UnsupportedType),
-    };
-
-    // Get the public CAs for the Signer.
-    let certs = signer.certs()?;
-    let sc_der_array_or_bytes = match certs.len() {
-        1 => Value::Bytes(certs[0].clone()), // single cert
-        _ => {
-            let mut sc_der_array: Vec<Value> = Vec::new();
-            for cert in certs {
-                sc_der_array.push(Value::Bytes(cert));
-            }
-            Value::Array(sc_der_array) // provide vec of certs when required
-        }
-    };
-
-    let mut unprotected = match signer.time_authority_url() {
-        Some(url) => {
-            let cts = cose_timestamp_countersign(data, &alg, &url)?;
-            let sigtst_vec = serde_cbor::to_vec(&make_cose_timestamp(&cts))?;
-            let sigtst_cbor = serde_cbor::from_slice(&sigtst_vec)?;
-
-            HeaderBuilder::new()
-                .text_value("x5chain".to_string(), sc_der_array_or_bytes)
-                .text_value("sigTst".to_string(), sigtst_cbor)
-        }
-        None => {
-            let sign_time = chrono::Utc::now().to_rfc3339(); // todo: remove when switch to cose_timestamp
-            HeaderBuilder::new()
-                .text_value("x5chain".to_string(), sc_der_array_or_bytes)
-                .text_value("temp_signing_time".to_string(), Value::Text(sign_time))
-        }
-    };
-
-    // set the ocsp responder response if available
-    if let Some(ocsp) = signer.ocsp_val() {
-        let mut ocsp_vec: Vec<Value> = Vec::new();
-        let mut r_vals: Vec<(Value, Value)> = vec![];
-
-        ocsp_vec.push(Value::Bytes(ocsp));
-        r_vals.push((Value::Text("ocspVals".to_string()), Value::Array(ocsp_vec)));
-
-        unprotected = unprotected.text_value("rVals".to_string(), Value::Map(r_vals));
-    }
-
     // build complete header
-    let unprotected_header = unprotected.build();
+    let (alg_id, unprotected_header) = build_unprotected_header(
+        data,
+        &alg,
+        signer.certs()?,
+        signer.time_authority_url(),
+        signer.ocsp_val(),
+    )?;
 
     let aad = b""; // no additional data required here
 
@@ -166,6 +178,73 @@ pub(crate) fn cose_sign(signer: &dyn Signer, data: &[u8], box_size: usize) -> Re
         .try_create_signature(aad, |bytes| signer.sign(bytes))?;
 
     let mut sign1 = sign1_builder.build();
+    sign1.payload = None; // clear the payload since it is known
+
+    let c2pa_sig_data = pad_cose_sig(&mut sign1, box_size)?;
+
+    // println!("sig: {}", Hexlify(&c2pa_sig_data));
+
+    Ok(c2pa_sig_data)
+}
+
+/// Returns signed Cose_Sign1 bytes for "data".  The Cose_Sign1 will be signed with the algorithm from `Signer`.
+#[cfg(feature = "async_signer")]
+pub async fn cose_sign_async(
+    signer: &dyn crate::AsyncSigner,
+    data: &[u8],
+    box_size: usize,
+) -> Result<Vec<u8>> {
+    // 13.2.1. X.509 Certificates
+    //
+    // X.509 Certificates are stored in a header named x5chain draft-ietf-cose-x509.
+    // The value is a CBOR array of byte strings, each of which contains the certificate
+    // encoded as ASN.1 distinguished encoding rules (DER). This array must contain at
+    // least one element. The first element of the array must be the certificate of
+    // the signer, and the subjectPublicKeyInfo element of the certificate will be the
+    // public key used to validate the signature. The Validity member of the TBSCertificate
+    // sequence provides the time validity period of the certificate.
+
+    /*
+       This header parameter allows for a single X.509 certificate or a
+       chain of X.509 certificates to be carried in the message.
+
+       *  If a single certificate is conveyed, it is placed in a CBOR
+           byte string.
+
+       *  If multiple certificates are conveyed, a CBOR array of byte
+           strings is used, with each certificate being in its own byte
+           string.
+    */
+
+    let alg = signer.alg().ok_or(Error::UnsupportedType)?;
+
+    // build complete header
+    let (alg_id, unprotected_header) = build_unprotected_header(
+        data,
+        &alg,
+        signer.certs()?,
+        signer.time_authority_url(),
+        signer.ocsp_val(),
+    )?;
+
+    let aad = b""; // no additional data required here
+
+    let sign1_builder = CoseSign1Builder::new()
+        .protected(alg_id)
+        .unprotected(unprotected_header)
+        .payload(data.to_vec());
+
+    let mut sign1 = sign1_builder.build();
+
+    let tbs = coset::sig_structure_data(
+        coset::SignatureContext::CoseSign1,
+        sign1.protected.clone(),
+        None,
+        aad,
+        sign1.payload.as_ref().unwrap_or(&vec![]),
+    );
+    sign1.signature = signer.sign(tbs).await?;
+
     sign1.payload = None; // clear the payload since it is known
 
     let c2pa_sig_data = pad_cose_sig(&mut sign1, box_size)?;
@@ -190,13 +269,13 @@ fn pad_cose_sig(sign1: &mut CoseSign1, end_size: usize) -> Result<Vec<u8>> {
         .map_err(|_e| Error::CoseSignature)?;
     let cur_size = cur_vec.len();
 
-    // check for box too small
-    match cur_size > end_size {
-        true => {
-            return Err(Error::CoseSigboxTooSmall);
-        }
-        false if cur_size == end_size => return Ok(cur_vec),
-        false => (),
+    if cur_size == end_size {
+        return Ok(cur_vec);
+    }
+
+    // check for box too small and matched size
+    if cur_size + PAD_OFFSET > end_size {
+        return Err(Error::CoseSigboxTooSmall);
     }
 
     let mut padding_found = false;
@@ -235,7 +314,7 @@ fn pad_cose_sig(sign1: &mut CoseSign1, end_size: usize) -> Result<Vec<u8>> {
         match new_cbor.len() < end_size {
             true => target_guess += 1,
             false if new_cbor.len() == end_size => return Ok(new_cbor),
-            false => break, // we couuld not match end_size in a single pad so break and add a second
+            false => break, // we could not match end_size in a single pad so break and add a second
         }
     }
 
