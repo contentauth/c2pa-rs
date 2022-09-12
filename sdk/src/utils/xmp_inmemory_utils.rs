4d78c122961c53f073d3d931e0d9975096eea1cd
Eric Scouten <scouten@adobe.com>
2022-09-12 16:56:09 -0700

    Update for new quick-xml API

--- sdk/src/utils/xmp_inmemory_utils.rs
+++ sdk/src/utils/xmp_inmemory_utils.rs
@@ -24,7 +24,7 @@ use crate::{
     Result,
 };
 
-const RDF_DESCRIPTION: &[u8] = b"rdf:Description";
+const RDF_DESCRIPTION: &str = "rdf:Description";
 
 #[derive(Default)]
 pub struct XmpInfo {
@@ -34,17 +34,19 @@ pub struct XmpInfo {
 }
 
 impl XmpInfo {
-    /// search xmp data for provenance, documentID and instanceID
+    /// Search XMP data for provenance, documentID, and instanceID fields
+    /// and construct a new instance.
     pub fn from_source(source: &mut dyn CAIRead, format: &str) -> Self {
         let xmp = get_cailoader_handler(format).and_then(|cai_loader| {
-            // read xmp if available
+            // Read XMP if available.
             cai_loader.read_xmp(source)
         });
 
-        // todo: do this in one pass through XMP
+        // TODO: Do this in one pass through XMP.
         let provenance = xmp.as_deref().and_then(extract_provenance);
         let document_id = xmp.as_deref().and_then(extract_document_id);
         let instance_id = xmp.as_deref().and_then(extract_instance_id);
+
         Self {
             document_id,
             instance_id,
@@ -53,42 +55,44 @@ impl XmpInfo {
     }
 }
 
-/// Extract an a value from XMP using a key
+/// Extract an a value from XMP using a key.
 fn extract_xmp_key(xmp: &str, key: &str) -> Option<String> {
     let mut reader = Reader::from_str(xmp);
     reader.trim_text(true);
-    let mut buf = Vec::new();
 
     loop {
-        match reader.read_event(&mut buf) {
+        match reader.read_event() {
             Ok(Event::Start(ref e)) | Ok(Event::Empty(ref e)) => {
-                if e.name() == RDF_DESCRIPTION {
-                    // attribute case
+                if e.name().as_ref() == RDF_DESCRIPTION.as_bytes() {
+                    // See if we have a matching attribute.
+
                     let value = e.attributes().find(|a| {
                         if let Ok(attribute) = a {
-                            vec_compare(attribute.key, key.as_bytes())
+                            vec_compare(attribute.key.as_ref(), key.as_bytes())
                         } else {
                             false
                         }
                     });
+
                     if let Some(Ok(attribute)) = value {
                         if let Ok(s) = String::from_utf8(attribute.value.to_vec()) {
                             return Some(s);
                         }
                     }
-                } else if e.name() == key.as_bytes() {
-                    // tag case
-                    let mut buf: Vec<u8> = Vec::new();
-                    if let Ok(s) = reader.read_text(e.name(), &mut buf) {
-                        return Some(s);
+                } else if e.name().as_ref() == key.as_bytes() {
+                    // Found a matching tag.
+                    if let Ok(s) = reader.read_text(e.name()) {
+                        return Some(s.into_owned());
                     }
                 }
             }
+
             Ok(Event::Eof) => break,
+
             _ => {}
         }
-        buf.clear();
     }
+
     None
 }
 
@@ -96,22 +100,24 @@ fn extract_xmp_key(xmp: &str, key: &str) -> Option<String> {
 fn add_xmp_key(xmp: &str, key: &str, value: &str) -> Result<String> {
     let mut reader = Reader::from_str(xmp);
     reader.trim_text(true);
+
     let mut writer = Writer::new(Cursor::new(Vec::new()));
-    let mut buf = Vec::new();
     let mut added = false;
+
     loop {
-        match reader.read_event(&mut buf) {
-            Ok(Event::Start(ref e)) if e.name() == RDF_DESCRIPTION => {
-                // creates a new element
-                let mut elem = BytesStart::owned(RDF_DESCRIPTION.to_vec(), RDF_DESCRIPTION.len());
+        match reader.read_event() {
+            Ok(Event::Start(ref e)) if e.name().as_ref() == RDF_DESCRIPTION.as_bytes() => {
+                // Creates a new element.
+                let mut elem = BytesStart::new(RDF_DESCRIPTION);
+
                 for attr in e.attributes() {
                     if let Ok(attr) = attr {
-                        if attr.key == key.as_bytes() {
-                            // replace the key/value if it exists
+                        if attr.key.as_ref() == key.as_bytes() {
+                            // Replace the key/value if it exists.
                             elem.push_attribute((key, value));
                             added = true;
                         } else {
-                            // add all other existing elements
+                            // Add all other existing elements.
                             elem.extend_attributes([attr]);
                         }
                     } else {
@@ -119,19 +125,24 @@ fn add_xmp_key(xmp: &str, key: &str, value: &str) -> Result<String> {
                         return Err(Error::XmpReadError);
                     }
                 }
+
                 if !added {
-                    // didn't exist, so add it
+                    // Element didn't previously exist, so add it.
                     elem.push_attribute((key, value));
                 }
-                // writes the event to the writer
+
+                // Write the event to the writer.
                 assert!(writer.write_event(Event::Start(elem)).is_ok());
             }
-            Ok(Event::End(ref e)) if e.name() == b"this_tag" => {
+
+            Ok(Event::End(ref e)) if e.name().as_ref() == b"this_tag" => {
                 assert!(writer
-                    .write_event(Event::End(BytesEnd::borrowed(b"my_elem")))
+                    .write_event(Event::End(BytesEnd::new("my_elem")))
                     .is_ok());
             }
+
             Ok(Event::Eof) => break,
+
             Ok(e) => assert!(writer.write_event(e).is_ok()),
             Err(e) => {
                 error!("Error at position {}: {:?}", reader.buffer_position(), e);
@@ -139,27 +150,29 @@ fn add_xmp_key(xmp: &str, key: &str, value: &str) -> Result<String> {
             }
         }
     }
-    buf.clear();
+
     let result = writer.into_inner().into_inner();
     String::from_utf8(result).map_err(|_e| Error::XmpWriteError)
 }
 
-/// extract the dc:provenance value from xmp
+/// Extract the `dc:provenance` value from an XMP packet.
 pub fn extract_provenance(xmp: &str) -> Option<String> {
     extract_xmp_key(xmp, "dcterms:provenance")
 }
 
-/// extract the xmpMM:InstanceID value from xmp
+/// Extract the `xmpMM:InstanceID` value from an XMP packet.
 fn extract_instance_id(xmp: &str) -> Option<String> {
     extract_xmp_key(xmp, "xmpMM:InstanceID")
 }
 
-/// extract the "xmpMM:DocumentID" value from xmp
+/// Extract the `xmpMM:DocumentID` value from an XP packet.
 fn extract_document_id(xmp: &str) -> Option<String> {
     extract_xmp_key(xmp, "xmpMM:DocumentID")
 }
 
-/// add or replace a dc:provenance value to xmp, including dc:terms if needed
+/// Add or replace a `dc:provenance` value in an XMP packet.
+///
+/// Add `dc:terms` namespace if needed.
 #[allow(dead_code)] // keep for future
 fn add_provenance(xmp: &str, provenance: &str) -> Result<String> {
     let xmp = add_xmp_key(xmp, "xmlns:dcterms", "http://purl.org/dc/terms/")?;
@@ -171,7 +184,6 @@ mod tests {
     #![allow(clippy::expect_used)]
     #![allow(clippy::unwrap_used)]
 
-    //use env_logger;
     use super::*;
 
     const XMP_DATA: &str = r#"<?xpacket begin="" id="W5M0MpCehiHzreSzNTczkc9d"?>
@@ -203,18 +215,22 @@ mod tests {
     fn read_xmp() {
         let provenance = extract_provenance(XMP_DATA);
         assert_eq!(provenance, Some("self#jumbf=c2pa/contentauth:urn:uuid:a58065fb-79ae-4eb3-87b9-a19830860059/c2pa.claim".to_owned()));
+
         let document_id = extract_document_id(XMP_DATA);
         assert_eq!(
             document_id,
             Some("xmp.did:cb9f5498-bb58-4572-8043-8c369e6bfb9b".to_owned())
         );
+
         let instance_id = extract_instance_id(XMP_DATA);
         assert_eq!(
             instance_id,
             Some("xmp.iid:cb9f5498-bb58-4572-8043-8c369e6bfb9b".to_owned())
         );
+
         let unicorn = extract_xmp_key(XMP_DATA, "unicorn");
         assert_eq!(unicorn, None);
+
         let bad_xmp = extract_xmp_key("bad xmp", "unicorn");
         assert_eq!(bad_xmp, None);
     }
