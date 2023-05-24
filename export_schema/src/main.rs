28e652f215e8382ed4b30fe83d2699b265087451
Dave Kozma <dkozma@adobe.com>
2023-05-24 19:35:57 -0400

    Remove needless borrow

--- export_schema/src/main.rs
+++ export_schema/src/main.rs
@@ -12,9 +12,9 @@ fn main() -> Result<()> {
     let schema = gen.into_root_schema_for::<ManifestStore>();
     let output = serde_json::to_string_pretty(&schema).expect("Failed to serialize schema");
     let output_dir = Path::new("./target/schema");
-    fs::create_dir_all(&output_dir).expect("Could not create schema directory");
+    fs::create_dir_all(output_dir).expect("Could not create schema directory");
     let output_path = output_dir.join("ManifestStore.schema.json");
     fs::write(&output_path, output).expect("Unable to write schema");
-    println!("Wrote schema to {}", &output_path.display());
+    println!("Wrote schema to {}", output_path.display());
     Ok(())
 }
