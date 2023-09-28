2635927780602859f2c9cb75e457208396ffd0cc
Eric Scouten <scouten@adobe.com>
2023-09-28 11:09:56 -0700

    (MINOR) Signer can call timestamp authority directly (#311)

--- sdk/src/signer.rs
+++ sdk/src/signer.rs
@@ -34,6 +34,18 @@ pub trait Signer {
         None
     }
 
+    /// Request RFC 3161 timestamp to be included in the manifest data
+    /// structure.
+    ///
+    /// `message` is a preliminary hash of the claim
+    ///
+    /// The default implementation will send the request to the URL
+    /// provided by [`Self::time_authority_url()`], if any.
+    fn send_timestamp_request(&self, message: &[u8]) -> Option<Result<Vec<u8>>> {
+        self.time_authority_url()
+            .map(|url| crate::time_stamp::default_rfc3161_request(&url, message))
+    }
+
     /// OCSP response for the signing cert if available
     /// This is the only C2PA supported cert revocation method.
     /// By pre-querying the value for a your signing cert the value can
@@ -97,6 +109,20 @@ pub trait AsyncSigner: Sync {
     /// URL for time authority to time stamp the signature
     fn time_authority_url(&self) -> Option<String> {
         None
+    }
+
+    /// Request RFC 3161 timestamp to be included in the manifest data
+    /// structure.
+    ///
+    /// `message` is a preliminary hash of the claim
+    ///
+    /// The default implementation will send the request to the URL
+    /// provided by [`Self::time_authority_url()`], if any.
+    async fn send_timestamp_request(&self, message: &[u8]) -> Option<Result<Vec<u8>>> {
+        // NOTE: This is currently synchronous, but may become
+        // async in the future.
+        self.time_authority_url()
+            .map(|url| crate::time_stamp::default_rfc3161_request(&url, message))
     }
 
     /// OCSP response for the signing cert if available
