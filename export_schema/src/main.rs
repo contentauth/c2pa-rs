4c62166d5c5f3fa87315a1016360d4f699648f4d
Dave Kozma <dkozma@adobe.com>
2024-06-12 13:33:39 -0400

    Generate schemas for ManifestStore and Settings

--- export_schema/src/main.rs
+++ export_schema/src/main.rs
@@ -1,17 +1,25 @@
 use std::{fs, path::Path};
 
 use anyhow::Result;
-use c2pa::settings::Settings;
-use schemars::schema_for;
+use c2pa::{settings::Settings, ManifestStore};
+use schemars::{schema::RootSchema, schema_for};
 
-fn main() -> Result<()> {
-    println!("Exporting JSON schema");
-    let schema = schema_for!(Settings);
-    let output = serde_json::to_string_pretty(&schema).expect("Failed to serialize schema");
+fn write_schema(schema: &RootSchema, name: &str) {
+    println!("Exporting JSON schema for {}", name);
+    let output = serde_json::to_string_pretty(schema).expect("Failed to serialize schema");
     let output_dir = Path::new("./target/schema");
     fs::create_dir_all(output_dir).expect("Could not create schema directory");
-    let output_path = output_dir.join("Settings.schema.json");
+    let output_path = output_dir.join(format!("{}.schema.json", name));
     fs::write(&output_path, output).expect("Unable to write schema");
     println!("Wrote schema to {}", output_path.display());
+}
+
+fn main() -> Result<()> {
+    let manifest_store = schema_for!(ManifestStore);
+    write_schema(&manifest_store, "ManifestStore");
+
+    let settings = schema_for!(Settings);
+    write_schema(&settings, "Settings");
+
     Ok(())
 }
