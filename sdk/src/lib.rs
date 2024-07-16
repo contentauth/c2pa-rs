2405cccb233af5ca2c30c911ce33b876d2d93ae2
nick <nick.libraries@gmail.com>
2024-07-16 01:18:09 -0400

    Expose `SignatureInfo` publicly (#501)
    * Expose `SignatureInfo` publicly
    
    * Make `SignatureInfo` ser/de fields public

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -121,7 +121,7 @@ pub use hash_utils::{hash_stream_by_alg, HashRange};
 pub use ingredient::Ingredient;
 #[cfg(feature = "file_io")]
 pub use ingredient::{DefaultOptions, IngredientOptions};
-pub use manifest::Manifest;
+pub use manifest::{Manifest, SignatureInfo};
 pub use manifest_assertion::{ManifestAssertion, ManifestAssertionKind};
 #[cfg(feature = "v1_api")]
 pub use manifest_store::ManifestStore;
