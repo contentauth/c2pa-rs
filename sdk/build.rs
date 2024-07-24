c0c56120d0ea091c51072f5858d3c3b0d42f0d22
ok-nick <nick.libraries@gmail.com>
2024-07-24 12:08:24 -0400

    Temp remove conditional comp for ci

--- sdk/build.rs
+++ sdk/build.rs
@@ -2,8 +2,6 @@ fn main() {
     // TODO: we don't want this to run during a normal debug build, check out:
     // https://github.com/rust-lang/cargo/issues/1581#issuecomment-1216924878
 
-    // We only need the submodule (assets) when testing.
-    #[cfg(debug_assertions)]
     std::process::Command::new("git")
         .args([
             "submodule",
