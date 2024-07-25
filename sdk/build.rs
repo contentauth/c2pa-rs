2a1df558054b19dafe9730ccbab7f1b783222916
ok-nick <nick.libraries@gmail.com>
2024-07-25 11:52:00 -0400

    Checkout submodules in CI

--- sdk/build.rs
+++ sdk/build.rs
@@ -2,16 +2,16 @@ fn main() {
     // TODO: we don't want this to run during a normal debug build, check out:
     // https://github.com/rust-lang/cargo/issues/1581#issuecomment-1216924878
 
-    std::process::Command::new("git")
-        .args([
-            "submodule",
-            "update",
-            "--init",
-            "--depth",
-            "1",
-            "--recommend-shallow",
-        ])
-        .current_dir(std::path::Path::new(env!("CARGO_MANIFEST_DIR")))
-        .output()
-        .expect("Failed to fetch git submodules!");
+    // std::process::Command::new("git")
+    //     .args([
+    //         "submodule",
+    //         "update",
+    //         "--init",
+    //         "--depth",
+    //         "1",
+    //         "--recommend-shallow",
+    //     ])
+    //     .current_dir(std::path::Path::new(env!("CARGO_MANIFEST_DIR")))
+    //     .output()
+    //     .expect("Failed to fetch git submodules!");
 }
