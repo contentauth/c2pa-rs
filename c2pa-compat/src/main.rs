f798c29f5dad319b40d3fb8169addddcb38cecca
ok-nick <nick.libraries@gmail.com>
2024-08-05 15:34:09 -0400

    Revert to old diff and upload artifacts for test

--- c2pa-compat/src/main.rs
+++ c2pa-compat/src/main.rs
@@ -148,12 +148,15 @@ fn main() -> Result<()> {
                     &mut signed_remote_asset,
                 )?;
 
-                let signed_remote_asset_patch = signed_remote_asset.into_inner();
-                // let signed_remote_asset_patch =
-                //     diffy::create_patch_bytes(&original_asset, &signed_remote_asset.into_inner())
-                //         .to_bytes();
+                let mut signed_remote_asset_patch = Vec::new();
+                bsdiff::diff(
+                    &original_asset,
+                    &signed_remote_asset.into_inner(),
+                    &mut signed_remote_asset_patch,
+                )
+                .expect("TODO");
                 asset_details.uncompressed_remote_size = Some(signed_remote_asset_patch.len());
-                // let signed_remote_asset_patch = lz4_flex::compress(&signed_remote_asset_patch);
+                let signed_remote_asset_patch = lz4_flex::compress(&signed_remote_asset_patch);
 
                 fs::write(dir_path.join("remote.patch"), signed_remote_asset_patch)?;
                 fs::write(dir_path.join("remote.c2pa"), remote_c2pa_manifest)?;
@@ -164,12 +167,26 @@ fn main() -> Result<()> {
             Err(err) => return Err(err),
         }
 
-        let signed_embedded_asset_patch = signed_embedded_asset.into_inner();
-        // let signed_embedded_asset_patch =
-        //     diffy::create_patch_bytes(&original_asset, &signed_embedded_asset.into_inner())
-        //         .to_bytes();
+        let mut signed_embedded_asset_patch = Vec::new();
+        bsdiff::diff(
+            &original_asset,
+            &signed_embedded_asset.into_inner(),
+            &mut signed_embedded_asset_patch,
+        )
+        .expect("TODO");
+
+        // TODO: temporary for testing
+        let mut original = Vec::new();
+        bsdiff::patch(
+            &original_asset,
+            &mut Cursor::new(&mut signed_embedded_asset_patch),
+            &mut original,
+        )
+        .unwrap();
+        fs::write(dir_path.join("embedded.original"), original)?;
+
         asset_details.uncompressed_embedded_size = Some(signed_embedded_asset_patch.len());
-        // let signed_embedded_asset_patch = lz4_flex::compress(&signed_embedded_asset_patch);
+        let signed_embedded_asset_patch = lz4_flex::compress(&signed_embedded_asset_patch);
 
         fs::write(dir_path.join("embedded.patch"), signed_embedded_asset_patch)?;
         fs::write(dir_path.join("embedded.c2pa"), embedded_c2pa_manifest)?;
