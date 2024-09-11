62424054316ce6883c4e7e76db25f9d6871185a8
Eli Mallon <eli@aquareum.tv>
2024-09-11 15:50:51 -0700

    reader: add get_provenance_cert_chain

--- sdk/src/reader.rs
+++ sdk/src/reader.rs
@@ -168,6 +168,11 @@ impl Reader {
         self.manifest_store.to_string()
     }
 
+    /// Return certificate chain for the provenance claim
+    pub fn get_provenance_cert_chain(&self) -> Result<String> {
+        self.manifest_store.store().get_provenance_cert_chain()
+    }
+
     /// Get the [`ValidationStatus`] array of the manifest store if it exists.
     ///
     /// This validation report only includes error statuses on applied to the active manifest.
