37ec3917e5031550d5d892a7bd3a849076fd593f
Gavin  Peacock <gpeacock@adobe.com>
2024-09-24 13:37:50 -0700

    Fixes issue where nested assertion-uri-hash errors were being reported at the active manifest level. (#593)
    * Improve reporting of ingredient errors by reporting full paths.
    * Fix Dedup of nested assertion errors.
    * Add Unit test for hashed-uri errors.
    * Add make_images script to generate test case.
    * Update and add Reader unit tests.
    Co-authored-by: Maurice Fisher <mfisher@adobe.com>

--- sdk/src/reader.rs
+++ sdk/src/reader.rs
@@ -270,3 +270,35 @@ impl std::fmt::Debug for Reader {
         f.write_str(&report.to_string())
     }
 }
+
+#[test]
+#[cfg(feature = "file_io")]
+fn test_reader_from_file_no_manifest() -> Result<()> {
+    let result = Reader::from_file("tests/fixtures/IMG_0003.jpg");
+    assert!(matches!(result, Err(Error::JumbfNotFound)));
+    Ok(())
+}
+
+#[test]
+#[cfg(feature = "file_io")]
+#[allow(clippy::unwrap_used)]
+fn test_reader_from_file_validation_err() -> Result<()> {
+    let reader = Reader::from_file("tests/fixtures/XCA.jpg")?;
+    assert!(reader.validation_status().is_some());
+    assert_eq!(
+        reader.validation_status().unwrap()[0].code(),
+        crate::validation_status::ASSERTION_DATAHASH_MISMATCH
+    );
+    Ok(())
+}
+
+#[test]
+#[cfg(feature = "file_io")]
+/// Test that the reader can validate a file with nested assertion errors
+fn test_reader_from_file_nested_errors() -> Result<()> {
+    let reader = Reader::from_file("tests/fixtures/CACAE-uri-CA.jpg")?;
+    println!("{reader}");
+    assert_eq!(reader.validation_status(), None);
+    assert_eq!(reader.manifest_store.manifests().len(), 3);
+    Ok(())
+}
