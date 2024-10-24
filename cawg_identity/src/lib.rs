b3b1553eaad64f2920d17cd1250c4ecfe14d573d
Eric Scouten <scouten@adobe.com>
2024-10-24 11:29:51 -0700

    feat: Create placeholder for forthcoming crate

---
+++ cawg_identity/src/lib.rs
@@ -0,0 +1,12 @@
+/// This crate is a placeholder. More to come soon ...
+pub fn does_nothing_yet() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_does_nothing() {
+        println!("Placeholder");
+    }
+}
