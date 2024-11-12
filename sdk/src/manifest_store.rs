0e3257722a2d13b18977a0046963e21439a602a8
Eric Scouten <scouten@adobe.com>
2024-11-11 19:34:58 -0800

    Test doc tests

--- sdk/src/manifest_store.rs
+++ sdk/src/manifest_store.rs
@@ -329,7 +329,7 @@ impl ManifestStore {
     /// # use c2pa::Result;
     /// use c2pa::ManifestStore;
     /// # fn main() -> Result<()> {
-    /// let manifest_store = ManifestStore::from_file("tests/fixtures/C.jpg")?;
+    /// let manifest_store = ManifestStore::from_file("tests/fixtures/Cx.jpg")?;
     /// println!("{}", manifest_store);
     /// # Ok(())
     /// # }
