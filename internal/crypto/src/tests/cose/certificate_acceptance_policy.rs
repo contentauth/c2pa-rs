8d58a9f9ab5ab47a99cd9e0301f65947d653bd4e
Eric Scouten <scouten@adobe.com>
2024-12-17 17:32:24 -0800

    Add coverage for the async flavor of `validate_certificate`

--- internal/crypto/src/tests/cose/certificate_acceptance_policy.rs
+++ internal/crypto/src/tests/cose/certificate_acceptance_policy.rs
@@ -18,7 +18,7 @@ use x509_parser::prelude::ExtendedKeyUsage;
 
 use crate::{
     cose::{CertificateAcceptancePolicy, CertificateValidationError, InvalidCertificateError},
-    raw_signature::signer::test_signer,
+    raw_signature::signer::{async_test_signer, test_signer},
     SigningAlg,
 };
 
@@ -264,6 +264,49 @@ fn test_trust_store() {
     cap.validate_certificate(&es512_certs[1..], &es512_certs[0], None)
         .unwrap();
     cap.validate_certificate(&ed25519_certs[1..], &ed25519_certs[0], None)
+        .unwrap();
+}
+
+#[actix::test]
+async fn test_trust_store_async() {
+    let cap = CertificateAcceptancePolicy::default();
+
+    let ps256 = async_test_signer(SigningAlg::Ps256);
+    let ps384 = async_test_signer(SigningAlg::Ps384);
+    let ps512 = async_test_signer(SigningAlg::Ps512);
+    let es256 = async_test_signer(SigningAlg::Es256);
+    let es384 = async_test_signer(SigningAlg::Es384);
+    let es512 = async_test_signer(SigningAlg::Es512);
+    let ed25519 = async_test_signer(SigningAlg::Ed25519);
+
+    let ps256_certs = ps256.cert_chain().unwrap();
+    let ps384_certs = ps384.cert_chain().unwrap();
+    let ps512_certs = ps512.cert_chain().unwrap();
+    let es256_certs = es256.cert_chain().unwrap();
+    let es384_certs = es384.cert_chain().unwrap();
+    let es512_certs = es512.cert_chain().unwrap();
+    let ed25519_certs = ed25519.cert_chain().unwrap();
+
+    cap.validate_certificate_async(&ps256_certs[1..], &ps256_certs[0], None)
+        .await
+        .unwrap();
+    cap.validate_certificate_async(&ps384_certs[1..], &ps384_certs[0], None)
+        .await
+        .unwrap();
+    cap.validate_certificate_async(&ps512_certs[1..], &ps512_certs[0], None)
+        .await
+        .unwrap();
+    cap.validate_certificate_async(&es256_certs[1..], &es256_certs[0], None)
+        .await
+        .unwrap();
+    cap.validate_certificate_async(&es384_certs[1..], &es384_certs[0], None)
+        .await
+        .unwrap();
+    cap.validate_certificate_async(&es512_certs[1..], &es512_certs[0], None)
+        .await
+        .unwrap();
+    cap.validate_certificate_async(&ed25519_certs[1..], &ed25519_certs[0], None)
+        .await
         .unwrap();
 }
 
