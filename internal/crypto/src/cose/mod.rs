d3f501bdba11f762bd8ed85973244967add3b23e
Eric Scouten <scouten@adobe.com>
2025-01-02 14:31:56 -0800

    feat: Add new function `c2pa_crypto::cose::signing_time_from_sign1` (#812)
    (Will allow me to make some other things private.)

--- internal/crypto/src/cose/mod.rs
+++ internal/crypto/src/cose/mod.rs
@@ -33,14 +33,17 @@ mod sign;
 pub use sign::{sign, sign_async};
 
 mod sign1;
-pub use sign1::{cert_chain_from_sign1, parse_cose_sign1, signing_alg_from_sign1};
+pub use sign1::{
+    cert_chain_from_sign1, parse_cose_sign1, signing_alg_from_sign1, signing_time_from_sign1,
+    signing_time_from_sign1_async,
+};
 
 mod sigtst;
 pub use sigtst::{
     add_sigtst_header, add_sigtst_header_async, cose_countersign_data, parse_and_validate_sigtst,
-    parse_and_validate_sigtst_async, validate_cose_tst_info, validate_cose_tst_info_async,
-    TstToken,
+    parse_and_validate_sigtst_async, TstToken,
 };
+pub(crate) use sigtst::{validate_cose_tst_info, validate_cose_tst_info_async};
 
 mod time_stamp_storage;
 pub use time_stamp_storage::TimeStampStorage;
