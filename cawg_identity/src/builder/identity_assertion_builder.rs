57e189edc975480c7a14a67f6c0371d8b567ba31
Tania Mathern <mathern@adobe.com>
2025-01-27 09:17:28 -0800

    ci: Format

--- cawg_identity/src/builder/identity_assertion_builder.rs
+++ cawg_identity/src/builder/identity_assertion_builder.rs
@@ -43,10 +43,10 @@ impl IdentityAssertionBuilder {
     // Create an `IdentityAssertionBuilder` for the given
     /// `CredentialHolder` pointer.
     pub fn for_credential_holder_boxed(credential_holder: Box<dyn CredentialHolder + Sync + Send>) -> Self {
-      Self {
-          credential_holder,
-      }
-  }
+        Self {
+            credential_holder,
+        }
+    }
 }
 
 #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
