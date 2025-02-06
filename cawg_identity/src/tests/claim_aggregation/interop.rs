2443c7593abebfe80d29553d8c2940c948d2c4cf
Tania Mathern <mathern@adobe.com>
2025-02-05 16:05:37 -0800

    ci: Clean up leftover debug code

--- cawg_identity/src/tests/claim_aggregation/interop.rs
+++ cawg_identity/src/tests/claim_aggregation/interop.rs
@@ -52,6 +52,7 @@ async fn adobe_connected_identities() {
 
     assert_eq!(ica_vc.verified_identities.len().get(), 1);
     let vi1 = ica_vc.verified_identities.first();
+    println!("ica_vc.verified_identities.first(): {:?}", vi1);
 
     assert_eq!(
         vi1,
