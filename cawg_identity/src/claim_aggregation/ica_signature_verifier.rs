6110a2bed558805f7e1effd22313fddb56457796
Gavin Peacock <gpeacock@adobe.com>
2025-03-20 14:53:03 -0700

    chore(cawg-identity): remove dbg statements

--- cawg_identity/src/claim_aggregation/ica_signature_verifier.rs
+++ cawg_identity/src/claim_aggregation/ica_signature_verifier.rs
@@ -173,8 +173,6 @@ impl SignatureVerifier for IcaSignatureVerifier {
                     ));
                 };
 
-                dbg!(&jwk_prop);
-
                 // OMG SO HACKY!
                 let Ok(jwk_json) = serde_json::to_string_pretty(jwk_prop) else {
                     return Err(ValidationError::SignatureError(
