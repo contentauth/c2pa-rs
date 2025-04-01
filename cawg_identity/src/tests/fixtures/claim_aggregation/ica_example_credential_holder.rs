4f80643116f2c2b5ae050ead1146fdc66f656778
Eric Scouten <scouten@adobe.com>
2025-04-01 16:34:12 -0700

    Generate an ICA credential with an invalid time stamp

--- cawg_identity/src/tests/fixtures/claim_aggregation/ica_example_credential_holder.rs
+++ cawg_identity/src/tests/fixtures/claim_aggregation/ica_example_credential_holder.rs
@@ -202,7 +202,7 @@ async fn ica_signing() {
 
     // Write the sample file.
     std::fs::write(
-        "src/tests/fixtures/claim_aggregation/ica_validation/valid_time_stamp.jpg",
+        "src/tests/fixtures/claim_aggregation/ica_validation/invalid_time_stamp.jpg",
         dest.get_ref(),
     )
     .unwrap();
