c1e6b9c4b21584bf316bcc14f2473b3eb8829454
Eric Scouten <scouten@adobe.com>
2025-04-01 12:29:52 -0700

    Generate an ICA VC whose DID document lacks an assertionMethod entry

--- cawg_identity/src/tests/fixtures/claim_aggregation/ica_example_credential_holder.rs
+++ cawg_identity/src/tests/fixtures/claim_aggregation/ica_example_credential_holder.rs
@@ -190,8 +190,8 @@ async fn ica_signing() {
 
     let jwk_id = serde_json::to_string(&jwk).unwrap();
     let jwk_base64 = c2pa_crypto::base64::encode(jwk_id.as_bytes());
-    // let issuer_did = format!("did:example:{jwk_base64}");
-    let issuer_did = "did:web:example.com".to_owned();
+    // let issuer_did = format!("did:jwk:{jwk_base64}");
+    let issuer_did = "did:web:cawg-test-data.github.io:test-case:no-assertion-method".to_owned();
 
     let ica_holder = IcaExampleCredentialHolder::from_async_raw_signer(cawg_raw_signer, issuer_did);
     let iab = AsyncIdentityAssertionBuilder::for_credential_holder(ica_holder);
@@ -204,7 +204,7 @@ async fn ica_signing() {
 
     // Write the sample file.
     std::fs::write(
-        "src/tests/fixtures/claim_aggregation/ica_validation/unresolvable_did.jpg",
+        "src/tests/fixtures/claim_aggregation/ica_validation/did_doc_without_assertion_method.jpg",
         dest.get_ref(),
     )
     .unwrap();
