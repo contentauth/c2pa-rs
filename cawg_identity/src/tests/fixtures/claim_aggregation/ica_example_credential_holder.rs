e64c26fc990f37cd338f4ccee998ec487d187738
Eric Scouten <scouten@adobe.com>
2025-04-02 11:22:11 -0700

    Generate a validFrom that is after the CAWG signer time stamp

--- cawg_identity/src/tests/fixtures/claim_aggregation/ica_example_credential_holder.rs
+++ cawg_identity/src/tests/fixtures/claim_aggregation/ica_example_credential_holder.rs
@@ -20,7 +20,7 @@ use c2pa_crypto::{
     raw_signature::{self, AsyncRawSigner},
 };
 use c2pa_status_tracker::StatusTracker;
-use chrono::{DateTime, FixedOffset, NaiveDate, TimeZone, Utc};
+use chrono::{DateTime, FixedOffset, NaiveDate, TimeDelta, TimeZone, Utc};
 use coset::{iana::OkpKeyParameter, RegisteredLabel};
 use iref::UriBuf;
 use nonempty_collections::{nev, NEVec};
@@ -109,7 +109,7 @@ impl AsyncCredentialHolder for IcaExampleCredentialHolder {
         // TO DO: Bring in substitute for now() on Wasm.
         #[cfg(not(target_arch = "wasm32"))]
         {
-            ica_vc.valid_from = Some(Utc::now().fixed_offset());
+            ica_vc.valid_from = Some(Utc::now().fixed_offset() + TimeDelta::new(60, 0).unwrap());
         }
 
         let ica_json = serde_json::to_string(&ica_vc).unwrap();
@@ -157,7 +157,7 @@ async fn ica_signing() {
         &cawg_cert_chain,
         &cawg_private_key,
         SigningAlg::Ed25519,
-        None,
+        Some("http://timestamp.digicert.com".to_string()),
     )
     .unwrap();
 
@@ -202,7 +202,7 @@ async fn ica_signing() {
 
     // Write the sample file.
     std::fs::write(
-        "src/tests/fixtures/claim_aggregation/ica_validation/valid_from_missing.jpg",
+        "src/tests/fixtures/claim_aggregation/ica_validation/valid_from_after_time_stamp.jpg",
         dest.get_ref(),
     )
     .unwrap();
