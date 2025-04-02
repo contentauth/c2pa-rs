676760809dc2d2eed5b944520cc6e14b86ce7147
Eric Scouten <scouten@adobe.com>
2025-04-02 09:51:03 -0700

    Generate credential with validFrom far in the future

--- cawg_identity/src/tests/fixtures/claim_aggregation/ica_example_credential_holder.rs
+++ cawg_identity/src/tests/fixtures/claim_aggregation/ica_example_credential_holder.rs
@@ -20,7 +20,7 @@ use c2pa_crypto::{
     raw_signature::{self, AsyncRawSigner},
 };
 use c2pa_status_tracker::StatusTracker;
-use chrono::{DateTime, FixedOffset, Utc};
+use chrono::{DateTime, FixedOffset, NaiveDate, TimeZone, Utc};
 use coset::{iana::OkpKeyParameter, RegisteredLabel};
 use iref::UriBuf;
 use nonempty_collections::{nev, NEVec};
@@ -109,7 +109,15 @@ impl AsyncCredentialHolder for IcaExampleCredentialHolder {
         // TO DO: Bring in substitute for now() on Wasm.
         #[cfg(not(target_arch = "wasm32"))]
         {
-            ica_vc.valid_from = Some(Utc::now().fixed_offset());
+            ica_vc.valid_from = Some(
+                NaiveDate::from_ymd_opt(2200, 1, 1)
+                    .unwrap()
+                    .and_hms_opt(12, 0, 0)
+                    .unwrap()
+                    .and_utc()
+                    .fixed_offset(),
+            );
+            // ica_vc.valid_from = Some(Utc::now().fixed_offset());
         }
 
         let ica_json = serde_json::to_string(&ica_vc).unwrap();
@@ -157,7 +165,7 @@ async fn ica_signing() {
         &cawg_cert_chain,
         &cawg_private_key,
         SigningAlg::Ed25519,
-        Some("http://timestamp.digicert.com".to_string()),
+        None,
     )
     .unwrap();
 
@@ -202,7 +210,7 @@ async fn ica_signing() {
 
     // Write the sample file.
     std::fs::write(
-        "src/tests/fixtures/claim_aggregation/ica_validation/invalid_time_stamp.jpg",
+        "src/tests/fixtures/claim_aggregation/ica_validation/valid_from_in_future.jpg",
         dest.get_ref(),
     )
     .unwrap();
