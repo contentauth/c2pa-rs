ab02a996ce09493736ebd9d63167b0c9a28924d7
Gavin  Peacock <gpeacock@adobe.com>
2025-05-13 16:24:55 -0700

    feat: Adds c_api for dynamic library releases (#1047)
    * feat: Adds c2pa_c library releases
    * Includes automation tools - run make release to build library artifacts
    * use share workplace version for sdk, c2pa_c and make_test_images
    * Be consistent about capitalisation and punctuation in comments
    * Update make_zip.rs
    * Update c2pa_stream.rs
    * chore: Rename c2pa_c folder to c_api
    * chore: Deflate make_zip files
    * feat: removes json_api
    behind a json_api feature if you need it.
    this includes the Rust functions:
    Sdk_Version()
    read_file, read_ingredient_file and sign_file.
    The C based API remains the same.
    Removes v1_api dependency.
    * fix(c_api): enhance error check macros
    prep json_api as feature for removal
    * feat: Add Builder::composed_manifest() static method
    * chore(c_api): update unit tests for new location
    More vigilant error handling
    * chore(c_api) Adds unit test coverage for c_api
    (except for json_api which will be dropped)
    * chore(c_api) More unit test coverage
    * chore(c2pa_c): update to upload-artifact@v4
    * chore(c2pa_c): revise build scripts for different architectures
    * chore: add windows dependencies
    * chore(c_api): move make_zip to Makefile, use native crypto
    * feat(c_api): Adds c api for set last error and adds colons to error messages.
    C API callbacks (streams & signers)  can now return error types and messages.
    This is a breaking change due to returning the colon in errors to separate the type.
    Some new error types are now mapped to Signature errors.
    * chore: review feedback fixes

---
+++ c_api/build.rs
@@ -0,0 +1,63 @@
+// Copyright 2023 Adobe. All rights reserved.
+// This file is licensed to you under the Apache License,
+// Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+// or the MIT license (http://opensource.org/licenses/MIT),
+// at your option.
+
+// Unless required by applicable law or agreed to in writing,
+// this software is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR REPRESENTATIONS OF ANY KIND, either express or
+// implied. See the LICENSE-MIT and LICENSE-APACHE files for the
+// specific language governing permissions and limitations under
+// each license.
+
+//! This creates the c2pa.h header file and the c2pa_version.txt file
+//! in the target directory. It is intended to be run as part of the build process.
+//! The crate version is added to the header file.
+use std::{env, path::Path};
+
+fn main() {
+    // Get the version from the environment variable set by Cargo.
+    let version = env::var("CARGO_PKG_VERSION").expect("CARGO_PKG_VERSION is not set");
+
+    // Get the workspace target directory.
+    let out_dir = env::var("OUT_DIR").expect("OUT_DIR environment variable not set");
+    println!("Running c_api build script: {:?}", out_dir);
+
+    let workspace_target_dir = Path::new(&out_dir)
+        .ancestors()
+        .nth(3)
+        .expect("Invalid OUT_DIR structure");
+
+    // Generate the bindings using cbindgen.
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set");
+    let config_path = Path::new(&crate_dir).join("cbindgen.toml");
+
+    let mut config = cbindgen::Config::from_file(config_path).unwrap();
+
+    // Add a version string to the header.
+    config.header = match config.header {
+        Some(ref mut header) => {
+            header.push_str(&format!("\n// Version: {}\n", version));
+            Some(header.clone())
+        }
+        None => Some(format!("\n// Version: {}\n", version)),
+    };
+
+    // Generate the header file.
+    cbindgen::generate_with_config(&crate_dir, config).map_or_else(
+        |error| match error {
+            cbindgen::Error::ParseSyntaxError { .. } => {
+                eprintln!("Warning: ParseSyntaxError encountered while generating bindings");
+            }
+            e => panic!("{:?}", e),
+        },
+        |bindings| {
+            println!(
+                "Writing c2pa.h to: {:?}",
+                workspace_target_dir.join("c2pa.h")
+            );
+            bindings.write_to_file(workspace_target_dir.join("c2pa.h"));
+        },
+    );
+}
