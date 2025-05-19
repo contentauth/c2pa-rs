4dc99a8d55105ca530a180f3342cb0a8e266db4e
mauricefisher64 <92736594+mauricefisher64@users.noreply.github.com>
2025-05-19 11:00:24 -0400

    remove unintentional file addition

--- sdk/src/store.rs
+++ sdk/src/store.rs
@@ -2273,6 +2273,7 @@ impl Store {
             };
         }
 
+        // sign the claim
         let pc = temp_store.provenance_claim().ok_or(Error::ClaimEncoding)?;
         let sig = temp_store.sign_claim(pc, signer, signer.reserve_size())?;
         let sig_placeholder = Store::sign_claim_placeholder(pc, signer.reserve_size());
