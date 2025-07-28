12282dea1370ed3d8c25dc32686adbd3bdda01b1
Alex Tran <alextrnnn@gmail.com>
2025-07-28 08:20:33 -0700

    fix: Conditionally run benchmarking

--- sdk/benches/signing.rs
+++ sdk/benches/signing.rs
@@ -1,4 +1,5 @@
 use std::io::Cursor;
+
 use c2pa::{Builder, CallbackSigner, SigningAlg};
 use criterion::{criterion_group, criterion_main, Criterion};
 const CERTS: &[u8] = include_bytes!("../tests/fixtures/certs/ed25519.pub");
@@ -9,13 +10,14 @@ fn signing(c: &mut Criterion) {
     let ed_signer =
         |_context: *const (), data: &[u8]| CallbackSigner::ed25519_sign(data, PRIVATE_KEY);
     let signer = CallbackSigner::new(ed_signer, SigningAlg::Ed25519, CERTS);
-    let mut source= Cursor::new(include_bytes!("fixtures/100_kb.jpg"));
+
+    let mut source = Cursor::new(include_bytes!("fixtures/100_kb.jpg"));
     let mut dest = Cursor::new(Vec::new());
     let format = "image/jpeg";
 
-    c.bench_function("sign 100kb jpeg", |b| b.iter(|| {
-        builder.sign(&signer, format,&mut source,&mut dest)
-    }));
+    c.bench_function("sign 100kb jpeg", |b| {
+        b.iter(|| builder.sign(&signer, format, &mut source, &mut dest))
+    });
 }
 
 criterion_group!(benches, signing);
