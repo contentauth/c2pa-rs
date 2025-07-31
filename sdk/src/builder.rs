c83632cb6df274fd49b32e0b7ecb2cef73bb0159
Alex Tran <alextrnnn@gmail.com>
2025-07-31 14:08:59 -0700

    fix: Add special case for metadata

--- sdk/src/builder.rs
+++ sdk/src/builder.rs
@@ -30,7 +30,8 @@ use crate::{
     assertion::AssertionDecodeError,
     assertions::{
         c2pa_action, labels, Action, ActionTemplate, Actions, AssertionMetadata, BmffHash, BoxHash,
-        CreativeWork, DataHash, EmbeddedData, Exif, SoftwareAgent, Thumbnail, User, UserCbor,
+        CreativeWork, DataHash, EmbeddedData, Exif, Metadata, SoftwareAgent, Thumbnail, User,
+        UserCbor,
     },
     cbor_types::value_cbor_to_type,
     claim::Claim,
@@ -877,6 +878,10 @@ impl Builder {
                 BmffHash::LABEL => {
                     let bmff_hash: BmffHash = manifest_assertion.to_assertion()?;
                     claim.add_assertion_with_salt(&bmff_hash, &salt)
+                }
+                l if l.ends_with(".metadata") => {
+                    let metadata: Metadata = manifest_assertion.to_assertion()?;
+                    claim.add_gathered_assertion_with_salt(&metadata, &salt)
                 }
                 _ => match &manifest_assertion.data {
                     AssertionData::Json(value) => claim.add_gathered_assertion_with_salt(
