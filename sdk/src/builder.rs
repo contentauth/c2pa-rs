b27f743cd5189a8ca56a877af0061c16d7bb0ade
Tania Mathern <mathern@adobe.com>
2025-09-18 17:01:32 -0700

    fix: Format

--- sdk/src/builder.rs
+++ sdk/src/builder.rs
@@ -2974,7 +2974,10 @@ mod tests {
             .await
             .expect("from_bytes");
         //println!("{reader}");
-        assert!(matches!(reader.validation_state(), ValidationState::Trusted | ValidationState::Valid));
+        assert!(matches!(
+            reader.validation_state(),
+            ValidationState::Trusted | ValidationState::Valid
+        ));
         let m = reader.active_manifest().unwrap();
         assert_eq!(m.ingredients().len(), 1);
         let parent = reader.get_manifest(parent_manifest_label).unwrap();
