9e95db869b8cc9c7806fce87d4844fe2c2a629a9
Gavin  Peacock <gpeacock@adobe.com>
2025-10-07 15:45:34 -0700

    fix: Empty actions array should be allowed for v1 claim compatibility [CAI-9573] (#1477)
    * fix: allow empty actions array for v1 claims [1413]

--- sdk/src/claim.rs
+++ sdk/src/claim.rs
@@ -2001,6 +2001,13 @@ impl Claim {
             )?;
         }
 
+        // Skip further checks for v1 claims if not in strict validation mode
+        if claim.version() == 1 {
+            if let Ok(false) = get_settings_value::<bool>("verify.strict_v1_validation") {
+                return Ok(()); // no further checks for v1 claims
+            }
+        }
+
         // 1. make sure every action has an actions array that is not empty
         if let Some(bad_assertion) = all_actions.iter().find(|a| {
             if let Ok(actions) = Actions::from_assertion(a.assertion()) {
