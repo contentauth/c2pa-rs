ddb3c9dfc80de0a436e169aa5fa25e5852223fc0
ok-nick <nick.libraries@gmail.com>
2025-12-10 11:29:38 -0500

    fix: remove extra hash binding verification for update manifests

--- sdk/src/store.rs
+++ sdk/src/store.rs
@@ -1725,12 +1725,6 @@ impl Store {
                         }
                     }
 
-                    // if this ingredient is the hash binding claim (update manifest)
-                    // then we have to check the binding here
-                    if ingredient.label() == svi.binding_claim {
-                        Claim::verify_hash_binding(ingredient, asset_data, svi, validation_log)?;
-                    }
-
                     Claim::verify_claim(
                         ingredient,
                         asset_data,
