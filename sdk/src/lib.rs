12c3f84bdb981889f3d43e77c7af75b98e50e11e
Eric Scouten <scouten@adobe.com>
2026-02-03 13:44:56 -0800

    feat: No-op change to trigger new build

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -152,7 +152,7 @@ pub mod settings;
 #[doc(hidden)]
 pub mod status_tracker;
 
-/// The validation_results module contains the definitions for the validation results that are part of the C2PA specification.
+/// Contains the definitions for the validation results that are part of the C2PA specification.
 pub mod validation_results;
 
 /// The validation_status module contains the definitions for the validation status that are part of the C2PA specification.
