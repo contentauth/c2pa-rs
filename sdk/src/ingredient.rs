a9a61436dc43354e43e05aa384427a62ab2ada31
ok-nick <nick.libraries@gmail.com>
2026-07-27 14:10:24 -0400

    fix: move new setting to separate PR

--- sdk/src/ingredient.rs
+++ sdk/src/ingredient.rs
@@ -660,7 +660,6 @@ impl Ingredient {
         result: Result<Store>,
         manifest_bytes: Option<Vec<u8>>,
         validation_log: &StatusTracker,
-        context: &Context,
     ) -> Result<()> {
         match result {
             Ok(store) => {
@@ -882,7 +881,7 @@ impl Ingredient {
         }
 
         // set validation status from result and log
-        self.update_validation_status(result, manifest_bytes, &validation_log, context)?;
+        self.update_validation_status(result, manifest_bytes, &validation_log)?;
 
         // create a thumbnail if we don't already have a manifest with a thumb we can use
         #[cfg(feature = "add_thumbnails")]
@@ -972,7 +971,7 @@ impl Ingredient {
             };
 
         // set validation status from result and log
-        ingredient.update_validation_status(result, manifest_bytes, &validation_log, context)?;
+        ingredient.update_validation_status(result, manifest_bytes, &validation_log)?;
 
         // create a thumbnail if we don't already have a manifest with a thumb we can use
         #[cfg(feature = "add_thumbnails")]
@@ -1464,12 +1463,7 @@ impl Ingredient {
             };
 
         // set validation status from result and log
-        ingredient.update_validation_status(
-            result,
-            Some(manifest_bytes),
-            &validation_log,
-            &context,
-        )?;
+        ingredient.update_validation_status(result, Some(manifest_bytes), &validation_log)?;
 
         // create a thumbnail if we don't already have a manifest with a thumb we can use
         #[cfg(feature = "add_thumbnails")]
