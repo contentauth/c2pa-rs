6cead3d029d0dd532f4a6c294a5f466415217fa8
mauricefisher64 <92736594+mauricefisher64@users.noreply.github.com>
2026-08-03 08:32:04 -0400

    Update sdk/src/spec_versions.rs
    Co-authored-by: tmathern <60901087+tmathern@users.noreply.github.com>

--- sdk/src/spec_versions.rs
+++ sdk/src/spec_versions.rs
@@ -21,4 +21,4 @@ pub const C2PA_VALIDATOR_VERSION: &str = "2.3.0";
 pub const C2PA_TSA_TRUST_URI: &str = "https://c2pa.org/trustlist-tsa";
 /// C2PA Trust URI
 #[allow(dead_code)]
-pub const C2PA_TRUST_URI: &str = "https://c2pa.org//trustlist";
+pub const C2PA_TRUST_URI: &str = "https://c2pa.org/trustlist";
