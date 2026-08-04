daaeb4c487bc60971f2f579dc0d5e4e4752016e0
mauricefisher64 <92736594+mauricefisher64@users.noreply.github.com>
2026-08-04 10:54:44 -0400

    Remove 2.4 feature

--- sdk/src/assertions/bmff_hash.rs
+++ sdk/src/assertions/bmff_hash.rs
@@ -384,9 +384,6 @@ pub struct BmffHash {
     #[serde(skip_serializing)]
     url: Option<UriT>, // deprecated in V2 and not to be used
 
-    #[serde(rename = "sequenceNumber", skip_serializing_if = "Option::is_none")]
-    sequence_number: Option<u64>,
-
     #[serde(skip)]
     pub(crate) merkle_uuid_boxes: Option<Vec<u8>>, // flattened list of UUID boxes
 
@@ -411,7 +408,6 @@ impl BmffHash {
             merkle: None,
             name: Some(name.to_string()),
             url,
-            sequence_number: None,
             merkle_uuid_boxes: None,
             merkle_uuid_boxes_insertion_point: 0,
             merkle_replacement_range: 0,
